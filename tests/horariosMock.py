23cfce9dcecbab3529fe2cc5f7c9e9c1e0436378
Francisco Pereira Guimarães <franciscop.guimaraes04@gmail.com>
2024-09-11 20:29:01 -0300

    feat: add horariosMock

---
+++ tests/horariosMock.py
@@ -0,0 +1,49 @@
+HORARIOS_CHRIS = '''
+{
+    "nomeDoProfessor": "Prof. Chris",
+    "horarioDeAtendimento": "08:00 - 10:00",
+    "periodo": "Integral",
+    "sala": 4,
+    "predio": 1
+}
+'''
+
+HORARIOS_LAURA = '''
+{
+    "nomeDoProfessor": "Prof. Maria",
+    "horarioDeAtendimento": "14:00 - 16:00",
+    "periodo": "Noturno",
+    "sala": 7,
+    "predio": 2
+}
+'''
+
+HORARIOS_XICO = '''
+{
+    "nomeDoProfessor": "Prof. João",
+    "horarioDeAtendimento": "10:00 - 12:00",
+    "periodo": "Integral",
+    "sala": 12,
+    "predio": 3
+}
+'''
+
+HORARIOS_PEDRO = '''
+{
+    "nomeDoProfessor": "Prof. Pedro",
+    "horarioDeAtendimento": "09:00 - 11:00",
+    "periodo": "Integral",
+    "sala": 15,
+    "predio": 4
+}
+'''
+
+HORARIOS_ANA = '''
+{
+    "nomeDoProfessor": "Prof. Ana",
+    "horarioDeAtendimento": "11:00 - 13:00",
+    "periodo": "Noturno",
+    "sala": 22,
+    "predio": 6
+}
+'''
